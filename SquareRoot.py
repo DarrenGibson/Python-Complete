339e46b0e6953f94f095bba2cd58917e94378552
Darren Gibson <25262337+DarrenGibson@users.noreply.github.com>
2018-08-13 00:27:23 -0700

    Add files via upload

---
+++ SquareRoot.py
@@ -0,0 +1,109 @@
+#==================================================================
+#						    Square Root
+#==================================================================
+
+''' 
+Date    : 12/Aug/18		
+Subject : Math
+Title   : Python Program to Find the Square Root			
+source  : https://www.programiz.com/python-programming/examples
+''' 
+
+num = float(input('Please enter a number:'))
+num_sqrt = num ** 0.5
+print('The square root of %0.3f is %0.3f'%(num, num_sqrt))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Python Program to calculate the square root
+
+# Note: change this value for a different result
+num = 8 
+
+# uncomment to take the input from the user
+#num = float(input('Enter a number: '))
+num_sqrt = num ** 0.5
+print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
