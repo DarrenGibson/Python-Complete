99435e22ac0fac5388e5b04756210c673e7ac773
Darren Gibson <25262337+DarrenGibson@users.noreply.github.com>
2018-08-12 21:51:26 -0700

    Add files via upload

---
+++ PrimeNumbers.py
@@ -0,0 +1,116 @@
+#==================================================================
+#						  Find Prime Numbers
+#==================================================================
+
+''' 
+Date :		08/12/18
+Subject:	Math
+Title:		Python Program to Check Prime Number	
+source:		https://www.programiz.com/python-programming/examples/prime-number
+''' 
+
+
+
+''' My Version '''
+
+
+
+'''
+data = int(input('Please enter a number: '))
+
+for i in range (1, 10):
+	
+	quotient = data / data
+	
+	if quotient %0:
+		print('{} is a prime number')
+	else:
+		print('{} is a primr number')
+
+'''
+
+
+
+''' Simple Version '''
+
+
+
+'''
+
+
+
+# Python program to check if the input number is prime or not
+num = 407
+
+# take input from the user
+# num = int(input('Enter a number: '))
+# prime numbers are greater than 1
+if num > 1:
+	# check for factors
+	for i in range(2, num):
+		if (num % i) == 0:
+			print(num,"is not a prime number"
+			print(i,'times', num//i,'is', num
+			break
+			
+	else:
+		print(num,'is a prime number')		
+
+# if input number is less than 
+# or equal to 1, it is not prime
+
+else:
+	print(num, 'is not a prime number)	
+
+
+
+'''
+
+
+
+# input version
+
+
+
+# Python program to check if the input number is prime or not
+num = int(input('Enter a number: '))
+# Prime numbers are greater
+if num > 1:	
+	# check for factors
+	for i in range(2, num):
+		if (num % i) == 0:
+			print(num,'is not a prime number')
+			print(i, 'times', nun//i, 'is', num)
+			break
+			
+
+			
+'''
+
+ive messed this up but i am to tired to fix this :(
+
+
+		else:
+			print(num,'is a prime number')
+# if input number is less than 
+# or equal to 1, it is not prime
+	else:
+		print(num, 'is not a prime number')
+			
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
