ee85d883c140d35d23e896cd67db82073f2d541d
Darren Gibson <25262337+DarrenGibson@users.noreply.github.com>
2018-08-13 01:22:42 -0700

    Add files via upload

---
+++ RandomNumberGen.py
@@ -0,0 +1,40 @@
+#==================================================================
+#					  Random Number Generator
+#==================================================================
+
+''' 
+Date    : 08/12/18
+Subject : Random Number Generator
+Title   : Python Program to Generate a Random Number
+source  : https://www.programiz.com/python-programming/examples/random-number
+''' 
+
+# Program to generate a random number between 0 and 9
+# import the random module
+
+for i in range(1, 100):
+	import random
+	print(random.randint(0, 9))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
